5620ea3a388abb032afb7dea5ada762f620cb437
jaffycoder <abubakarjafaru1@gmail.com>
2023-03-18 13:51:41 +0000

    added puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual 
- * puzzle followed by a newline.
+ * main - prints "Programming is like building a multilingual
+ * puzzle followed 
+ * by a newline.
  * Return: Always 0.
  */
 
