3392d11410e04f4ccb27a7c2a989f7a02f2754c7
jaffycoder <abubakarjafaru1@gmail.com>
2023-03-18 14:01:01 +0000

    adds puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle followed 
- * by a newline.
+ * puzzle followed by a newline.
  * Return: Always 0.
  */
 
 int main(void)
 
 {
-	puts("\programming is like building a multilingual puzzle");
-	
+	puts("\Programming is like building a multilingual puzzle");
 	return (0);
 }
