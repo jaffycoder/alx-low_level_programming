14f7b14d23a86eb5268e32b7c50a18c7655bfd4c
jaffycoder <abubakarjafaru1@gmail.com>
2023-08-08 06:46:15 +0000

    added argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * agrstostr - convert the params passed to the program to string
+ * argstostr - convert the params passed to the program to string
  * @ac: the argument count
  * @av: the argument vector
  *
