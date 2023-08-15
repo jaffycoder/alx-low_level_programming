0f98c12a0504ea8e1257c67f714782ba5d61f07a
jaffycoder <abubakarjafaru1@gmail.com>
2023-08-15 04:47:05 +0000

    added dog again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - initializes a variable of type structure dog.
- * @d: pointer to the struct dog toget initialized
- * @name: name to be initialize
- * @age: age to be initailized
- * @owner: owner to be initialized
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
 
 struct dog
@@ -16,14 +18,13 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for a dog structure
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
