86c6bfd3ad672fc211f1b1a7d13a11de461bd302
jaffycoder <abubakarjafaru1@gmail.com>
2023-08-16 09:28:41 +0000

    added git pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - return index place if comparison = t -1
+ * @array: nothing more than an array
+ * @size: size of element in array
+ * @cmp: pointer of function of one of the 3 in main.
+ *
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
